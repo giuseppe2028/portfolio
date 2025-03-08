9456929e966a6fabdb338f69236dd6adedddc9ea
Giuseppe2028 <giuseppebarone2028@gmail.com>
2025-03-08 08:02:19 +0100

    fix: update path

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // IMPORTANTE: deve corrispondere al nome della repo GitHub
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
